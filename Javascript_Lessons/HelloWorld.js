34a27baccab10db1449fbf21d3c3eea4ab3370ac
kirtikumar5486 <kirti.itengg@gmail.com>
2024-09-21 15:08:19 -0400

    merge

--- Javascript_Lessons/HelloWorld.js
+++
@@ -1,5 +0,0 @@
-function executeScript() {
-    alert('How are you Krishiv , How are you Batman');
-    console.log("How are you Krishiv");
-    // You can add more code here to execute when the button is clicked
-}
